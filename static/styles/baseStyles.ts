56567fbabf82cbae5ab37cafb4fa8e9bf7274b0b
Oscar Peace <oscar@oscarcp.net>
2026-06-19 17:46:46 +0100

    feat: add extra pages to vite

--- static/styles/baseStyles.ts
+++ static/styles/baseStyles.ts
@@ -3,7 +3,7 @@ import { css } from "lit";
 export const baseStyles = css`
 @font-face {
   font-family: 'win-95-font';
-  src: url('./W95FA.otf') format('truetype');
+  src: url("../fonts/WIN95FA.otf") format('truetype');
   font-weight: normal;
   font-style: normal;
 }
